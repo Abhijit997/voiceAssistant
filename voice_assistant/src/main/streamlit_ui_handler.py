7eda7697b8f0ed555d230836d94bd63a653159d4
Abhijit Banerjee <44857651+Abhijit997@users.noreply.github.com>
2025-07-02 04:13:30 +0000

    temp

--- voice_assistant/src/main/streamlit_ui_handler.py
+++ voice_assistant/src/main/streamlit_ui_handler.py
@@ -8,6 +8,12 @@ import streamlit as st
 import streamlit_ui_home_page
 import streamlit_ui_login
 
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip_address = s.getsockname()[0]
+print(ip_address)
+
 NEO4J_URI = st.secrets['NEO4J_URI']
 NEO4J_USER = st.secrets['NEO4J_USER']
 NEO4J_PASSWORD = st.secrets['NEO4J_PASSWORD']
